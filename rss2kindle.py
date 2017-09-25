6d0a7e58280966b6ae5d22a882e365966f27fc68
Jean-Philippe CARUANA <jp.caruana@deepki.com>
2017-09-25 18:31:19 +0200

    conversion to python 3

--- rss2kindle.py
+++ rss2kindle.py
@@ -51,8 +51,8 @@ FEED_TIMEOUT = 60
 PROXY = ""
 
 
-import urllib2
-urllib2.install_opener(urllib2.build_opener())
+#import urllib2
+#urllib2.install_opener(urllib2.build_opener())
 
 ### Load the Options ###
 
@@ -69,8 +69,7 @@ warn = sys.stderr
 
 ### Import Modules ###
 
-from types import DictType
-import cPickle as pickle, os, traceback
+import pickle, os, traceback
 
 unix = 0
 try:
@@ -105,35 +104,31 @@ class InputError(Exception):
     pass
 
 
-def timelimit(timeout, function):
-    def internal2(*args, **kw):
-        """
-        from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/473878
-        """
+def timelimit(timeout):
+    def internal(function):
+        def internal2(*args, **kw):
+            class Calculator(threading.Thread):
+                def __init__(self):
+                    threading.Thread.__init__(self)
+                    self.result = None
+                    self.error = None
 
-        class Calculator(threading.Thread):
-            def __init__(self):
-                threading.Thread.__init__(self)
-                self.result = None
-                self.error = None
+                def run(self):
+                    try:
+                        self.result = function(*args, **kw)
+                    except:
+                        self.error = sys.exc_info()[0]
 
-            def run(self):
-                try:
-                    self.result = function(*args, **kw)
-                except:
-                    self.error = sys.exc_info()
-
-        c = Calculator()
-        c.setDaemon(True) # don't hold up exiting
-        c.start()
-        c.join(timeout)
-        if c.isAlive():
-            raise TimeoutError
-        if c.error:
-            raise c.error[0], c.error[1]
-        return c.result
-
-    return internal2
+            c = Calculator()
+            c.start()
+            c.join(timeout)
+            if c.isAlive():
+                raise TimeoutError
+            if c.error:
+                raise c.error
+            return c.result
+        return internal2
+    return internal
 
 
 def isstr(f):
@@ -150,7 +145,7 @@ def getID(entry):
     if TRUST_GUID:
         if 'id' in entry and entry.id:
             # Newer versions of feedparser could return a dictionary
-            if type(entry.id) is DictType:
+            if isinstance(entry.id, dict):
                 return entry.id.values()[0]
 
             return entry.id
@@ -169,13 +164,13 @@ class Feed:
 
 def load(lock=1):
     if not os.path.exists(feedfile):
-        print 'Feedfile "%s" does not exist.  If you\'re using r2e for the first time, you' % feedfile
-        print "have to run 'r2e new' first."
+        print ('Feedfile "%s" does not exist.  If you\'re using r2e for the first time, you' % feedfile)
+        print ("have to run 'r2e new' first.")
         sys.exit(1)
     try:
         feedfileObject = open(feedfile, 'r')
-    except IOError, e:
-        print "Feedfile could not be opened: %s" % e
+    except IOError as e:
+        print ("Feedfile could not be opened: %s" % e)
         sys.exit(1)
     feeds = pickle.load(feedfileObject)
 
@@ -211,11 +206,11 @@ def unlock(feeds, feedfileObject):
 
 #@timelimit(FEED_TIMEOUT)
 def parse(url, etag, modified):
-    if PROXY == '':
-        return feedparser.parse(url, etag, modified)
-    else:
-        proxy = urllib2.ProxyHandler({"http": PROXY})
-        return feedparser.parse(url, etag, modified, handlers=[proxy])
+    #if PROXY == '':
+    return feedparser.parse(url, etag, modified)
+    #else:
+    #    proxy = urllib2.ProxyHandler({"http": PROXY})
+    #    return feedparser.parse(url, etag, modified, handlers=[proxy])
 
 
 ### Program Functions ###
@@ -240,78 +235,75 @@ def ifeeds(feeds, num):
     return ifeeds
 
 
-def read_later(link):
+async def read_later(link):
     params = {'username': WALLABAG_USERNAME,
-              'password': WALLABAG_PASSWORD,
-              'client_id': WALLABAG_CLIENT_ID,
-              'client_secret': WALLABAG_CLIENT_SECRET}
+        'password': WALLABAG_PASSWORD,
+        'client_id': WALLABAG_CLIENT_ID,
+        'client_secret': WALLABAG_CLIENT_SECRET}
     token = await Wallabag.get_token(host=WALLABAG_URL, **params)
 
     async with aiohttp.ClientSession(loop=loop) as session:
         wall = Wallabag(host=WALLABAG_URL,
-                        client_secret=params.get('client_secret'),
-                        client_id=params.get('client_id'),
-                        token=token,
-                        aio_sess=session)
-
-        url = 'https://foxmask.trigger-happy.eu'
-        title = 'foxmask\'s  blog'
+            client_secret=params.get('client_secret'),
+            client_id=params.get('client_id'),
+            token=token,
+            aio_sess=session)
 
         try:
             await wall.post_entries(link, '', '', 0, 0)
-            print "   send %s" % (link)
+            print ("   send %s" % (link))
         except e:
-            print "   failure in sending %s: %s" % (link, e)
+            print ("   failure in sending %s: %s" % (link, e))
 
 
 def print_error(exc_type, feed, feednum, http_headers, http_result, http_status):
     if http_status not in [200, 302]:
-        print >> warn, "W: error %d [%d] %s" % (http_status, feednum, feed.url)
+        print ("W: error %d [%d] %s" % (http_status, feednum, feed.url))
 
     elif contains(http_headers.get('content-type', 'rss'), 'html'):
-        print >> warn, "W: looks like HTML [%d] %s" % (feednum, feed.url)
+        print ("W: looks like HTML [%d] %s" % (feednum, feed.url))
 
     elif http_headers.get('content-length', '1') == '0':
-        print >> warn, "W: empty page [%d] %s" % (feednum, feed.url)
+        print ("W: empty page [%d] %s" % (feednum, feed.url))
 
     elif hasattr(socket, 'timeout') and exc_type == socket.timeout:
-        print >> warn, "W: timed out on [%d] %s" % (feednum, feed.url)
+        print ("W: timed out on [%d] %s" % (feednum, feed.url))
 
     elif exc_type == IOError:
-        print >> warn, 'W: "%s" [%d] %s' % (http_result.bozo_exception, feednum, feed.url)
+        print ('W: "%s" [%d] %s' % (http_result.bozo_exception, feednum, feed.url))
 
     elif hasattr(feedparser, 'zlib') and exc_type == feedparser.zlib.error:
-        print >> warn, "W: broken compression [%d] %s" % (feednum, feed.url)
+        print ("W: broken compression [%d] %s" % (feednum, feed.url))
 
     elif exc_type in socket_errors:
         exc_reason = http_result.bozo_exception.args[1]
-        print >> warn, "W: %s [%d] %s" % (exc_reason, feednum, feed.url)
+        print ("W: %s [%d] %s" % (exc_reason, feednum, feed.url))
 
     elif exc_type == urllib2.URLError:
         if http_result.bozo_exception.reason.__class__ in socket_errors:
             exc_reason = http_result.bozo_exception.reason.args[1]
         else:
             exc_reason = http_result.bozo_exception.reason
-        print >> warn, "W: %s [%d] %s" % (exc_reason, feednum, feed.url)
+        print ("W: %s [%d] %s" % (exc_reason, feednum, feed.url))
 
     elif exc_type == AttributeError:
-        print >> warn, "W: %s [%d] %s" % (http_result.bozo_exception, feednum, feed.url)
+        print ("W: %s [%d] %s" % (http_result.bozo_exception, feednum, feed.url))
 
     elif exc_type == KeyboardInterrupt:
         raise http_result.bozo_exception
 
     elif http_result.bozo:
-        print >> warn, 'E: error in [%d] "%s" feed (%s)' % (
-            feednum, feed.url, http_result.get("bozo_exception", "can't process"))
+        print ('E: error in [%d] "%s" feed (%s)' % (
+                    feednum, feed.url, http_result.get("bozo_exception", "can't process")))
 
     else:
-        print >> warn, "=== rss2kindle encountered a problem with this feed ==="
-        print >> warn, "E:", http_result.get("bozo_exception", "can't process"), feed.url
-        print >> warn, http_result
-        print >> warn, "rss2kindle", __version__
-        print >> warn, "feedparser", feedparser.__version__
-        print >> warn, "Python", sys.version
-        print >> warn, "=== END HERE ==="
+        print ("=== rss2kindle encountered a problem with this feed ===")
+        print ("E:", http_result.get("bozo_exception", "can't process"), feed.url)
+        print (http_result)
+        print ("rss2kindle", __version__)
+        print ("feedparser", feedparser.__version__)
+        print ("Python", sys.version)
+        print ("=== END HERE ===")
 
 
 def run(num=None):
@@ -323,12 +315,12 @@ def run(num=None):
                     continue
 
                 if VERBOSE:
-                    print >> warn, 'I: Processing [%d] "%s"' % (feednum, feed.url)
+                    print ('I: Processing [%d] "%s"' % (feednum, feed.url))
                 http_result = {}
                 try:
                     http_result = timelimit(FEED_TIMEOUT, parse)(feed.url, feed.etag, feed.modified)
                 except TimeoutError:
-                    print >> warn, 'W: feed [%d] "%s" timed out' % (feednum, feed.url)
+                    print ('W: feed [%d] "%s" timed out' % (feednum, feed.url))
                     continue
 
                 # Handle various status conditions, as required
@@ -336,13 +328,13 @@ def run(num=None):
                     if http_result.status == 301:
                         feed.url = http_result['url']
                     elif http_result.status == 410:
-                        print >> warn, "W: feed gone; deleting", feed.url
+                        print ("W: feed gone; deleting", feed.url)
                         feeds.remove(feed)
                         continue
 
                 http_status = http_result.get('status', 200)
                 if VERBOSE > 1:
-                    print >> warn, "I: http status", http_status
+                    print ("I: http status", http_status)
                 if http_status != 304 and not http_result.entries and not http_result.get('version', ''):
                     http_headers = http_result.get('headers',{'content-type': 'application/rss+xml', 'content-length': '1'})
                     exc_type = http_result.get("bozo_exception", Exception()).__class__
@@ -361,7 +353,7 @@ def run(num=None):
                     frameid = entry.get('id')
                     if not (frameid):
                         frameid = id
-                    if type(frameid) is DictType:
+                    if isinstance(frameid, dict):
                         frameid = frameid.values()[0]
 
                     # If this item's ID is in our database
@@ -383,13 +375,13 @@ def run(num=None):
             except (KeyboardInterrupt, SystemExit):
                 raise
             except:
-                print >> warn, "=== rss2kindle encountered a problem with this feed ==="
-                print >> warn, "E: could not parse", feed.url
+                print ("=== rss2kindle encountered a problem with this feed ===")
+                print ("E: could not parse", feed.url)
                 traceback.print_exc(file=warn)
-                print >> warn, "rss2kindle", __version__
-                print >> warn, "feedparser", feedparser.__version__
-                print >> warn, "Python", sys.version
-                print >> warn, "=== END HERE ==="
+                print ("rss2kindle", __version__)
+                print ("feedparser", feedparser.__version__)
+                print ("Python", sys.version)
+                print ("=== END HERE ===")
                 continue
 
     finally:
@@ -401,7 +393,7 @@ def list():
     i = 0
     for feed in feeds:
         active = ('[ ]', '[*]')[feed.active]
-        print `i` + ':', active, feed.url
+        print (i + ':', active, feed.url)
         i += 1
 
 
@@ -411,22 +403,21 @@ def opmlexport():
     feeds, feedfileObject = load(lock=0)
 
     if feeds:
-        print '<?xml version="1.0" encoding="UTF-8"?>\n<opml version="1.0">\n<head>\n<title>rss2kindle OPML export</title>\n</head>\n<body>'
+        print ('<?xml version="1.0" encoding="UTF-8"?>\n<opml version="1.0">\n<head>\n<title>rss2kindle OPML export</title>\n</head>\n<body>')
         for f in feeds[1:]:
             url = xml.sax.saxutils.escape(f.url)
-            print '<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
-        print '</body>\n</opml>'
-
+            print ('<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url))
+        print ('</body>\n</opml>')
 
 def opmlimport(importfile):
     importfileObject = None
-    print 'Importing feeds from', importfile
+    print ('Importing feeds from', importfile)
     if not os.path.exists(importfile):
-        print 'OPML import file "%s" does not exist.' % importfile
+        print ('OPML import file "%s" does not exist.' % importfile)
     try:
         importfileObject = open(importfile, 'r')
-    except IOError, e:
-        print "OPML import file could not be opened: %s" % e
+    except IOError as e:
+        print ("OPML import file could not be opened: %s" % e)
         sys.exit(1)
     try:
         import xml.dom.minidom
@@ -434,7 +425,7 @@ def opmlimport(importfile):
         dom = xml.dom.minidom.parse(importfileObject)
         newfeeds = dom.getElementsByTagName('outline')
     except:
-        print 'E: Unable to parse OPML file'
+        print ('E: Unable to parse OPML file')
         sys.exit(1)
 
     feeds, feedfileObject = load(lock=1)
@@ -444,7 +435,7 @@ def opmlimport(importfile):
     for feed in newfeeds:
         if feed.hasAttribute('xmlUrl'):
             feedurl = feed.getAttribute('xmlUrl')
-            print 'Adding %s' % xml.sax.saxutils.unescape(feedurl)
+            print ('Adding %s' % xml.sax.saxutils.unescape(feedurl))
             feeds.append(Feed(feedurl))
 
     unlock(feeds, feedfileObject)
@@ -453,26 +444,26 @@ def opmlimport(importfile):
 def delete(n):
     feeds, feedfileObject = load()
     if (n == 0) and (feeds and isstr(feeds[0])):
-        print >> warn, "W: ID has to be equal to or higher than 1"
+        print ("W: ID has to be equal to or higher than 1")
     elif n >= len(feeds):
-        print >> warn, "W: no such feed"
+        print ("W: no such feed")
     else:
-        print >> warn, "W: deleting feed %s" % feeds[n].url
+        print ("W: deleting feed %s" % feeds[n].url)
         feeds = feeds[:n] + feeds[n + 1:]
         if n != len(feeds):
-            print >> warn, "W: feed IDs have changed, list before deleting again"
+            print ("W: feed IDs have changed, list before deleting again")
     unlock(feeds, feedfileObject)
 
 
 def toggleactive(n, active):
     feeds, feedfileObject = load()
     if (n == 0) and (feeds and isstr(feeds[0])):
-        print >> warn, "W: ID has to be equal to or higher than 1"
+        print ("W: ID has to be equal to or higher than 1")
     elif n >= len(feeds):
-        print >> warn, "W: no such feed"
+        print ("W: no such feed")
     else:
         action = ('Pausing', 'Unpausing')[active]
-        print >> warn, "%s feed %s" % (action, feeds[n].url)
+        print ("%s feed %s" % (action, feeds[n].url))
         feeds[n].active = active
     unlock(feeds, feedfileObject)
 
@@ -485,7 +476,7 @@ def reset():
         ifeeds = feeds
     for feed in ifeeds:
         if VERBOSE:
-            print "Resetting %d already seen items" % len(feed.seen)
+            print ("Resetting %d already seen items" % len(feed.seen))
         feed.seen = {}
         feed.etag = None
         feed.modified = None
@@ -499,31 +490,31 @@ def new_feedfile(feedfile):
 
 def delete_feed(action, args):
     if not args:
-        raise InputError, "Action '%s' requires an argument" % action
+        raise InputError( "Action '%s' requires an argument" % action)
     elif args[0].isdigit():
         delete(int(args[0]))
     else:
-        raise InputError, "Action '%s' requires a number as its argument" % action
+        raise InputError("Action '%s' requires a number as its argument" % action)
 
 
 def pause(action, args):
     global active
     if not args:
-        raise InputError, "Action '%s' requires an argument" % action
+        raise InputError("Action '%s' requires an argument" % action)
     elif args[0].isdigit():
         active = (action == "unpause")
         toggleactive(int(args[0]), active)
     else:
-        raise InputError, "Action '%s' requires a number as its argument" % action
+        raise InputError("Action '%s' requires a number as its argument" % action)
 
 
 def main(args):
     global feedfile, action, read_later, active, e
-    print "--------------------------------------------------------------------"
-    print "rss2kindle started"
+    print ("--------------------------------------------------------------------")
+    print ("rss2kindle started")
     try:
         if len(args) < 3:
-            raise InputError, "insufficient args"
+            raise InputError("insufficient args")
 
         feedfile, action, args = args[1], args[2], args[3:]
 
@@ -531,7 +522,7 @@ def main(args):
             if args and args[0] == "--no-send":
                 def read_later(link):
                     if VERBOSE:
-                        print 'Not sending:', link
+                        print ('Not sending:', link)
 
             if args and args[-1].isdigit():
                 run(int(args[-1]))
@@ -548,7 +539,7 @@ def main(args):
             list()
 
         elif action in ("help", "--help", "-h"):
-            print __doc__
+            print (__doc__)
 
         elif action == "delete":
             delete_feed(action, args)
@@ -564,19 +555,19 @@ def main(args):
 
         elif action == "opmlimport":
             if not args:
-                raise InputError, "OPML import requires a filename argument"
+                raise InputError("OPML import requires a filename argument")
             opmlimport(args[0])
 
         else:
-            raise InputError, "Invalid action"
+            raise InputError("Invalid action")
 
-    except InputError, e:
-        print "E:", e
-        print
-        print __doc__
+    except InputError as e:
+        print ("E:", e)
+        print('')
+        print (__doc__)
 
-    print "rss2kindle ended"
-    print "--------------------------------------------------------------------"
+    print ("rss2kindle ended")
+    print ("--------------------------------------------------------------------")
 
 if __name__ == '__main__':
     main(sys.argv)
